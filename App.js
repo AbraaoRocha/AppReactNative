bc2ca90969679f18d1aada89cc18c18152cc2ec9
Abraão Rocha <abraaorochafranca@gmail.com>
2020-11-01 19:21:13 -0300

    Final aula 06 - Navegação entre Telas

--- App.js
+++ App.js
@@ -1,48 +1,19 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, Button, Alert } from 'react-native';
-import Page from './views/Page';
-import {css} from './assets/css/css'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './views/Home';
+import Login from './views/Login';
 
 export default function App() {
-  
-  const [product,setProduct] = useState('qualquer');
-  
-  const [quantity,setQuantity] = useState(0);
 
-  useEffect(()=>{
-    
-    if(quantity>0){
-      
-      Alert.alert('Novo Produto Adicionado')
-    
-    }
+  const Stack = createStackNavigator();
 
-  }, [quantity]);
-
-  const props = {
-
-    empresa: 'AR Sofwares',
-    
-    name: 'Abraão Rocha',
-
-    produto: product,
-
-    quantidade: quantity
-  
-  };
-  
   return (
-    
-    <View style={css.container}>
-      
-      <Text style={css.textPage} > {product} {quantity} </Text>
-      
-      <Page {... props} />
-
-      <Button title='Adicionar Produto' onPress={()=>{setQuantity(quantity+1)}}/>
-
-    </View>
-  
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
-
-};
+}
